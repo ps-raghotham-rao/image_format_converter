b68a50bf4bd3418c938c21836f1f225021487e25
P S Raghotham Rao <73742827+ps-raghotham-rao@users.noreply.github.com>
2021-11-02 11:03:46 +0530

    Code written using functions

--- tif_to_other_format_converter_using_pillow.py
+++ tif_to_other_format_converter_using_pillow.py
@@ -3,27 +3,28 @@
 import glob
 from PIL import Image
 
+def tif_image_converter(old_format,new_format, img_path, path_to_store_converted_images):
+
+    # old_format - The image format which is to be converted, is appended to img_path.
+    path = glob.glob(img_path+"\*."+str(old_format))
+    
+    # adding backslash to the file path. As raw string does not allow direct '\' adding at the end. So, import os can be used or directly we can append.
+    path_to_store_converted_images+="\\"
+
+    for file in path:
+        file_name = file.split('\\')[-1].split('.')[0] + "." + new_format
+        path_to_store_images_with_file_name = path_to_store_converted_images+file_name
+        # print(path_to_store_images_with_file_name)
+        im = Image.open(file)
+        print(im.save(path_to_store_images_with_file_name))
 
 
 # Old Format - Existing image format. New format - The desirable format which you want to conver the image to.
 old_format = "tif"
 new_format = "jpg"
 
-# path is where you give the location of the images that is to be converted.
-path = glob.glob(r"C:\Users\Raghotham\Downloads\image_format_converter\*"+"."+str(old_format))
+# img_path - the location of the images that is to be converted.
+img_path = r"C:\Users\raghotham\Documents\stargan"
+path_to_store_converted_images = r"C:\Users\raghotham\Documents\stargan\new_format_folder"
 
-# path_to_store_converted_images is where you give the location to store converted images.
-path_to_store_converted_images = r"C:\Users\Raghotham\Downloads\image_format_converter\new_format_folder"
-
-# adding backslash to the file path. As raw string does not allow direct '\' adding at the end. So, import os can be used or directly we can append.
-path_to_store_converted_images+="\\"
-
-
-
-for file in path:
-    file_name = file.split('\\')[-1].split('.')[0] + "." + new_format
-    path_to_store_images_with_file_name = path_to_store_converted_images+file_name
-    print(path_to_store_images_with_file_name)
-    im = Image.open(file)
-    im.save(path_to_store_images_with_file_name)
-    
+tif_image_converter(old_format,new_format, img_path, path_to_store_converted_images)
